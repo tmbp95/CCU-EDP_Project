f7794b226409fa7d85c6052dcf8d4444f3b8618c
Rui G <16254422+hexaminer@users.noreply.github.com>
2018-01-14 13:36:27 +0000

    UPDATE to same configurations used along development
    UPDATE to same configurations used along development
    
    Now are this configurations are local
    - MySql (db.tecnico.ulisboa.pt/ist174270)
    - Remote API (85.247.219.175/explorer)

--- devops/01-runOnceAfterVMDeploy.sh
+++ devops/01-runOnceAfterVMDeploy.sh
@@ -9,10 +9,26 @@ sudo chown root.root /etc/profile.d/nodejs.sh
 sudo chmod go-wx /etc/profile.d/nodejs.sh
 
 export PATH=$PATH:~/.npm-global/bin
-npm install -g grunt-cli yo 
-# I commented this, because we need to decide which generator should be used
-# (ex. ng-fullstack generator-rest generator-angular)
-# npm install -g ng-fullstack
+npm install -g loopback-cli http-server
+
+# mysql install
+
+echo "mysql-server mysql-server/root_password password vagrant" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password vagrant" | sudo debconf-set-selections
+
+sudo apt-get install mysql-server -y
+
+sudo sed -i -e 's/127\.0\.0\.1/0\.0\.0\.0/g' /etc/mysql/my.cnf
+sudo service mysql restart
+
+mysql -u root --password=vagrant -e "CREATE USER ist174270@'%' IDENTIFIED BY 'vagrant';"
+mysql -u root --password=vagrant -e "GRANT ALL ON ist174270.* TO 'ist174270'@'%';"
+mysql -u ist174270 --password=vagrant -e "CREATE DATABASE ist174270;"
+
+mysql -u ist174270 --password=vagrant ist174270 < /home/vagrant/ccu/backend/sql/DBdumpCcuBroker.SQL
+
+#Change port of backend to the same of remote api developemnt
+sudo sed -i -e 's/3000/4567/g' /home/vagrant/ccu/backend/server/config.json
 
 # Atom additional addons/plugins (packages)
 apm install platformio-ide-terminal
