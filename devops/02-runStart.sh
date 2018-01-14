555728d93e5a4c4f7bcd4260386c0fa5786ab3e0
Rui G <16254422+hexaminer@users.noreply.github.com>
2018-01-14 13:39:27 +0000

    ADD to Devops 02-runStart.sh to start EDP Broker (after deployment and runonce)
    Script that will start backend and frontend services

---
+++ devops/02-runStart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /home/vagrant/ccu/backend
+npm install
+node . &
+
+cd /home/vagrant/ccu/client/site
+http-server &
+
+echo 'To access EDP Broker (Producers) use "http://localhost:8080/" or "http://85.247.219.175:8080/".'
+
+exit 0
